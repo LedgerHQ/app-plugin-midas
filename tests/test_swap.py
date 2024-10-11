ab76aaa7b7889778f3c2e87cead26e7b4aac394c
kostyamospan <kostya04042001@gmail.com>
2024-10-11 14:57:03 +0300

    chore: playground tests, tests refactoring

--- tests/test_swap.py
+++ tests/test_swap.py
@@ -11,71 +11,95 @@ import ledger_app_clients.ethereum.response_parser as ResponseParser
 from ledger_app_clients.ethereum.utils import get_selector_from_data, recover_transaction
 from ragger.navigator import NavInsID
 
-from .utils import get_appname_from_makefile, DERIVATION_PATH
-import logging
+from .utils import get_appname_from_makefile, DERIVATION_PATH, load_contracts, prepare_tx_params_deposit_instant, prepare_tx_params_deposit_request, prepare_tx_params_redeem_instant, prepare_tx_params_redeem_request
 
+import logging
 
 ROOT_SCREENSHOT_PATH = Path(__file__).parent
 ABIS_FOLDER = "%s/abis" % (os.path.dirname(__file__))
 
 PLUGIN_NAME = get_appname_from_makefile()
 
-print('NAME', PLUGIN_NAME)
 
 logging.basicConfig(level=logging.DEBUG)
 
-with open("%s/0x99361435420711723aF805F08187c9E6bF796683.abi.json" % (ABIS_FOLDER)) as file:
-    contract = Web3().eth.contract(
-        abi=json.load(file),
-        # Get address from filename
-        address=bytes.fromhex(os.path.basename(file.name).split(".")[0].split("x")[-1])
-    )
 
-
-
-# EDIT THIS: build your own test
-def test_deposit_instant(backend, firmware, navigator, test_name, wallet_addr):
+def test_deposit_instant_playground(backend, firmware, navigator, test_name, wallet_addr):
     client = EthAppClient(backend)
 
-    data = contract.encode_abi("depositInstant", args=[
-        "0xdAC17F958D2ee523a2206206994597C13D831ec7",
-        Web3.to_wei(100, "ether"),
-        Web3.to_wei(0, "ether"),
-        bytes.fromhex("00" * 32)
-    ])
-
-    # first setup the external plugin
-    client.set_external_plugin(PLUGIN_NAME,
-                               contract.address,
-                               # Extract function selector from the encoded data
-                               get_selector_from_data(data))
-
-    tx_params = {
-        "nonce": 20,
-        "maxFeePerGas": Web3.to_wei(145, "gwei"),
-        "maxPriorityFeePerGas": Web3.to_wei(1.5, "gwei"),
-        "gas": 173290,
-        "to": contract.address,
-        "value": Web3.to_wei(0, "ether"),
-        "chainId": ChainId.ETH,
-        "data": data
-    }
+    tx_params = prepare_tx_params_deposit_instant(client)
     # send the transaction
     with client.sign(DERIVATION_PATH, tx_params):
-        print("TEST")
-        # Validate the on-screen request by performing the navigation appropriate for this device
-        # if firmware.is_nano:
-        #     navigator.navigate_until_text(NavInsID.RIGHT_CLICK,
-        #                                               [NavInsID.BOTH_CLICK],
-        #                                               "Accept")
-        # else:
-        #     navigator.navigate_until_text_and_compare(NavInsID.SWIPE_CENTER_TO_LEFT,
-        #                                               [NavInsID.USE_CASE_REVIEW_CONFIRM,
-        #                                                NavInsID.USE_CASE_STATUS_DISMISS],
-        #                                               "Hold to sign",
-        #                                               ROOT_SCREENSHOT_PATH,
-        #                                               test_name)
+        print("Playground")
+
     # verify signature
     vrs = ResponseParser.signature(client.response().data)
     addr = recover_transaction(tx_params, vrs)
     assert addr == wallet_addr.get()
+
+
+
+def test_redeem_instant_playground(backend, firmware, navigator, test_name, wallet_addr):
+    client = EthAppClient(backend)
+
+    tx_params = prepare_tx_params_redeem_instant(client)
+    # send the transaction
+    with client.sign(DERIVATION_PATH, tx_params):
+        print("Playground")
+
+    # verify signature
+    vrs = ResponseParser.signature(client.response().data)
+    addr = recover_transaction(tx_params, vrs)
+    assert addr == wallet_addr.get()
+
+def test_deposit_request_playground(backend, firmware, navigator, test_name, wallet_addr):
+    client = EthAppClient(backend)
+
+    tx_params = prepare_tx_params_deposit_request(client)
+    # send the transaction
+    with client.sign(DERIVATION_PATH, tx_params):
+        print("Playground")
+
+    # verify signature
+    vrs = ResponseParser.signature(client.response().data)
+    addr = recover_transaction(tx_params, vrs)
+    assert addr == wallet_addr.get()
+
+
+def test_redeem_request_playground(backend, firmware, navigator, test_name, wallet_addr):
+    client = EthAppClient(backend)
+
+    tx_params = prepare_tx_params_redeem_request(client)
+    # send the transaction
+    with client.sign(DERIVATION_PATH, tx_params):
+        print("Playground")
+
+    # verify signature
+    vrs = ResponseParser.signature(client.response().data)
+    addr = recover_transaction(tx_params, vrs)
+    assert addr == wallet_addr.get()
+
+
+# def test_redeem_instant_playground(backend, firmware, navigator, test_name, wallet_addr):
+#     client = EthAppClient(backend)
+
+#     tx_params = prepare_tx_params_redeem_instant(client)
+#     # send the transaction
+#     with client.sign(DERIVATION_PATH, tx_params):
+#         print("TEST")
+#         # Validate the on-screen request by performing the navigation appropriate for this device
+#         # if firmware.is_nano:
+#         #     navigator.navigate_until_text(NavInsID.RIGHT_CLICK,
+#         #                                               [NavInsID.BOTH_CLICK],
+#         #                                               "Accept")
+#         # else:
+#         #     navigator.navigate_until_text_and_compare(NavInsID.SWIPE_CENTER_TO_LEFT,
+#         #                                               [NavInsID.USE_CASE_REVIEW_CONFIRM,
+#         #                                                NavInsID.USE_CASE_STATUS_DISMISS],
+#         #                                               "Hold to sign",
+#         #                                               ROOT_SCREENSHOT_PATH,
+#         #                                               test_name)
+#     # verify signature
+#     vrs = ResponseParser.signature(client.response().data)
+#     addr = recover_transaction(tx_params, vrs)
+#     assert addr == wallet_addr.get()
--- tests/utils.py
+++ tests/utils.py
@@ -2,9 +2,16 @@ import os
 import re
 from pathlib import Path
 from typing import Optional
+import json
 
+from web3 import Web3
+
+from eth_typing import ChainId
 from ledger_app_clients.ethereum.client import EthAppClient
 import ledger_app_clients.ethereum.response_parser as ResponseParser
+from ledger_app_clients.ethereum.utils import get_selector_from_data, recover_transaction
+from ragger.navigator import NavInsID
+
 
 DERIVATION_PATH = "m/44'/60'/0'/0/0"
 makefile_relative_path = "../Makefile"
@@ -14,6 +21,9 @@ makefile_path = (Path(os.path.dirname(os.path.realpath(__file__))) / Path(makefi
 pattern = r'.*APPNAME.*=.*'
 
 default_strip_parameter = " \t\n\r\x0b\x0c"
+
+ABIS_FOLDER = "%s/abis" % (os.path.dirname(__file__))
+
 
 
 def get_appname_from_makefile() -> str:
@@ -43,3 +53,137 @@ class WalletAddr:
         with self.client.get_public_addr(display=False, bip32_path=path):
             pass
         return ResponseParser.pk_addr(self.client.response().data)[1]
+
+class Object(object):
+    pass
+
+def load_contract(m_product_name, addr):
+    with open(f"{ABIS_FOLDER}/{m_product_name}/{addr}.abi.json") as file:
+        contract = Web3().eth.contract(
+            abi=json.load(file),
+            # Get address from filename
+            address=bytes.fromhex(os.path.basename(file.name).split(".")[0].split("x")[-1])
+        )
+    return contract
+
+def load_contracts():
+    obj = Object()
+    
+    obj.mTBillDepositVaultContract = load_contract("mTBILL","0x99361435420711723aF805F08187c9E6bF796683")
+    obj.mTBillRedemptionVaultContract = load_contract("mTBILL","0xF6e51d24F4793Ac5e71e0502213a9BBE3A6d4517")
+    obj.mTBillRedemptionVaultBuidlContract = load_contract("mTBILL","0x569D7dccBF6923350521ecBC28A555A500c4f0Ec")
+    
+    obj.mBasisDepositVaultContract = load_contract("mBASIS","0xa8a5c4FF4c86a459EBbDC39c5BE77833B3A15d88")
+    obj.mBasisRedemptionVaultContract = load_contract("mBASIS","0x19AB19e61A930bc5C7B75Bf06cDd954218Ca9F0b")
+    obj.mBasisRedemptionVaultSwapperContract = load_contract("mBASIS","0x0D89C1C4799353F3805A3E6C4e1Cbbb83217D123")
+    
+    return obj
+
+contracts = load_contracts()
+PLUGIN_NAME = get_appname_from_makefile()
+
+
+def prepare_tx_params_deposit_instant(client, params = None, depositVault = contracts.mTBillDepositVaultContract):
+    data = depositVault.encode_abi("depositInstant", args=[
+        getattr(params, 'tokenAddress', "0xdAC17F958D2ee523a2206206994597C13D831ec7"),
+        Web3.to_wei(getattr(params, 'tokenAmount', 100), "ether"),
+        Web3.to_wei(getattr(params, 'minToReceive', 0), "ether"),
+        bytes.fromhex(getattr(params, 'referrerId', "00" * 32)),
+    ])
+
+    # first setup the external plugin
+    client.set_external_plugin(PLUGIN_NAME,
+                               depositVault.address,
+                               # Extract function selector from the encoded data
+                               get_selector_from_data(data))
+
+    tx_params = {
+        "nonce": 20,
+        "maxFeePerGas": Web3.to_wei(145, "gwei"),
+        "maxPriorityFeePerGas": Web3.to_wei(1.5, "gwei"),
+        "gas": 173290,
+        "to": depositVault.address,
+        "value": Web3.to_wei(0, "ether"),
+        "chainId": ChainId.ETH,
+        "data": data
+    }
+
+    return tx_params
+
+def prepare_tx_params_deposit_request(client, params = None, depositVault = contracts.mTBillDepositVaultContract):
+    data = depositVault.encode_abi("depositRequest", args=[
+        getattr(params, 'tokenAddress', "0xdAC17F958D2ee523a2206206994597C13D831ec7"),
+        Web3.to_wei(getattr(params, 'tokenAmount', 100), "ether"),
+        bytes.fromhex(getattr(params, 'referrerId', "00" * 32)),
+    ])
+
+    # first setup the external plugin
+    client.set_external_plugin(PLUGIN_NAME,
+                               depositVault.address,
+                               # Extract function selector from the encoded data
+                               get_selector_from_data(data))
+
+    tx_params = {
+        "nonce": 20,
+        "maxFeePerGas": Web3.to_wei(145, "gwei"),
+        "maxPriorityFeePerGas": Web3.to_wei(1.5, "gwei"),
+        "gas": 173290,
+        "to": depositVault.address,
+        "value": Web3.to_wei(0, "ether"),
+        "chainId": ChainId.ETH,
+        "data": data
+    }
+
+    return tx_params
+
+
+def prepare_tx_params_redeem_instant(client, params = None, redeemVault = contracts.mTBillRedemptionVaultContract):
+    data = redeemVault.encode_abi("redeemInstant", args=[
+        getattr(params, 'tokenAddress', "0xdAC17F958D2ee523a2206206994597C13D831ec7"),
+        Web3.to_wei(getattr(params, 'mTokenAmount', 10), "ether"),
+        Web3.to_wei(getattr(params, 'minToReceive', 0), "ether")
+    ])
+
+    # first setup the external plugin
+    client.set_external_plugin(PLUGIN_NAME,
+                               redeemVault.address,
+                               # Extract function selector from the encoded data
+                               get_selector_from_data(data))
+
+    tx_params = {
+        "nonce": 20,
+        "maxFeePerGas": Web3.to_wei(145, "gwei"),
+        "maxPriorityFeePerGas": Web3.to_wei(1.5, "gwei"),
+        "gas": 173290,
+        "to": redeemVault.address,
+        "value": Web3.to_wei(0, "ether"),
+        "chainId": ChainId.ETH,
+        "data": data
+    }
+
+    return tx_params
+
+def prepare_tx_params_redeem_request(client, params = None, redeemVault = contracts.mTBillRedemptionVaultContract):
+    data = redeemVault.encode_abi(getattr(params, 'requestFn', "redeemRequest"), args=[
+        getattr(params, 'tokenAddress', "0xdAC17F958D2ee523a2206206994597C13D831ec7"),
+        Web3.to_wei(getattr(params, 'mTokenAmount', 100), "ether"),
+    ])
+
+    # first setup the external plugin
+    client.set_external_plugin(PLUGIN_NAME,
+                               redeemVault.address,
+                               # Extract function selector from the encoded data
+                               get_selector_from_data(data))
+
+    tx_params = {
+        "nonce": 20,
+        "maxFeePerGas": Web3.to_wei(145, "gwei"),
+        "maxPriorityFeePerGas": Web3.to_wei(1.5, "gwei"),
+        "gas": 173290,
+        "to": redeemVault.address,
+        "value": Web3.to_wei(0, "ether"),
+        "chainId": ChainId.ETH,
+        "data": data
+    }
+
+    return tx_params
